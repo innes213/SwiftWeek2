414df6569d98de90c14bcb76cf80a85d6b8e4559
Rob Innes Hislop <rob.hislop@rd.io>
2015-04-26 11:51:53 -0700

    Initial check-in, 4th time is the charm
    Restarting Swift class Week 2 assignment for the 4th time
    * Pods installed
    * table view added

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Rob Hislop on 4/26/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
